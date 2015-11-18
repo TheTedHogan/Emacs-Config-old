0f371e5e90c1669ff095887686d8d3788da276af
Ted Hogan <ted.hogan17@gmail.com>
2015-11-17 19:55:42 -0700

    Added YAML mode

--- init.el
+++ init.el
@@ -89,9 +89,9 @@
 ;;
 ;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
 ;; then you can add the following code to this file:
-;;
-;; (require 'yaml-mode)
-;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 ;; 
 ;; Adding this code will make Emacs enter yaml mode whenever you open
 ;; a .yml file
